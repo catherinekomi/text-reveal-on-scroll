6065cd385bc0edd3dad50db8fe3460ffa7a7d2c3
Catherine Komi <oceanbeaats@gmail.com>
2022-02-23 23:11:14 -0800

    done

--- app.js
+++ app.js
@@ -10,11 +10,25 @@ let observer = new IntersectionObserver(showItem, options);
 function showItem(entries){
     entries.forEach(entry => {
         if(entry.isIntersecting){
+            //each letter code
+            let letters = [...entry.target.querySelectorAll('span')];
+            letters.forEach((letter, idx) => {
+                setTimeout(()=>{
+                    letter.classList.add('active');
+                }, idx * 10)
+            })
+            // finish each letter code
             entry.target.children[0].classList.add('active');
         }
     })
 }
 
 listItems.forEach(item => {
-    observer.observe(item);
+//each letter code
+  let newString = '';
+  let itemText = item.children[0].innerText.split('');
+  itemText.map(letter =>(newString += letter == '' ?`<span class='gap'></span>`:`<span>${letter}</span>`))
+  item.innerHTML= newString; 
+  // finish each letter code
+  observer.observe(item);
 })
